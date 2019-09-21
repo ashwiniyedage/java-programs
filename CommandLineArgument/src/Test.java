9ee8a524b0185db3a8c03e812362803e9d5206dc
ash <yedgeashwini@gmail.com>
2019-09-21 18:36:15 +0530

    commit6

---
+++ CommandLineArgument/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String args[])
+	{
+		// TODO Auto-generated method stub
+		String s1=args[0];
+		String s2=args[1];
+		int n1=Integer.parseInt(s1);
+		int n2=Integer.parseInt(s2);
+		int sum=n1+n2;
+		System.out.println("Addition is :"+sum);
+	}
+
+}
